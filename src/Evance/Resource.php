01258fa32017d3c5f285b062170b00115d2270d5
Geoff Willings <geoff@azexis.com>
2017-12-19 16:33:53 +0000

    Updates and improvements to Service Resource class

--- src/Evance/Resource.php
+++ src/Evance/Resource.php
@@ -3,18 +3,38 @@
 namespace Evance;
 
 use GuzzleHttp\Psr7\Request;
+use Webmozart\Assert\Assert;
 
+/**
+ * Class Resource
+ * @package Evance
+ */
 class Resource
 {
+    /** @var App */
     private $client;
 
+    /**
+     * Resource constructor.
+     * @param App $client
+     */
     public function __construct(App $client)
     {
         $this->client = $client;
     }
 
+    /**
+     * @param string $httpVerb  A GET, POST, PUT or DELETE Http verb.
+     * @param string $url The Url of the API endpoint.
+     * @param array|null $params Optional array of parameters to send to the API endpoint.
+     * @return mixed
+     */
     public function call($httpVerb, $url, $params = null)
     {
+        Assert::oneOf($httpVerb, ['GET', 'POST', 'PUT', 'DELETE'], __METHOD__ . " encountered an unexpected HTTP verb '{$httpVerb}'");
+        Assert::nullOrIsArray($params, __METHOD__ . ' expects call parameters to be null or an array');
+        Assert::string($url, __METHOD__ . ' expects the $url to be provided as a string');
+
         $uri = $this->client->getResourceUri($url);
         $request = new Request(
             $httpVerb,
--- src/Evance/Service/Shipping/Resource/Services.php
+++ src/Evance/Service/Shipping/Resource/Services.php
@@ -3,32 +3,79 @@
 namespace Evance\Service\Shipping\Resource;
 
 use Evance\Resource;
+use Webmozart\Assert\Assert;
 
+/**
+ * The Shipping Service resource class is a direct representation of the API endpoint.
+ * If you do not wish to use our
+ * @package Evance\Service\Shipping\Resource
+ */
 class Services extends Resource
 {
 
-
+    /**
+     * Create/register a Shipping Service for the App.
+     * When creating a Shipping Service the service will automatically be associated with the App.
+     * @param array $properties The properties to send to the API.
+     * @return array
+     */
     public function add($properties)
     {
-        return $this->call('POST', '/shipping/services.json', ['shippingService' => $properties]);
+        Assert::isArray($properties, __METHOD__ . ' expects $properties to be supplied as an array');
+        if (!array_key_exists('shippingService', $properties)) {
+            throw new \InvalidArgumentException(__METHOD__ . ' expected a "shippingService" property');
+        }
+        return $this->call('POST', '/shipping/services.json', $properties);
     }
 
-
+    /**
+     * Delete a Shipping Service for the App.
+     * Note: you can only delete a Shipping Service if the App is the owner of the service.
+     * @param int $id The ID of the Shipping Service.
+     * @return array
+     */
     public function delete($id)
     {
-
+        Assert::integerish($id, __METHOD__ . 'expects $id to be an integer');
+        return $this->call('DELETE', "/shipping/services/{$id}.json");
     }
 
-
+    /**
+     * Get a Shipping Service for the App.
+     * Note: you may only GET Shipping Service information for services owned by the App.
+     * @param int $id The ID of the Shipping Service.
+     * @return array
+     */
     public function get($id)
     {
-
+        Assert::integerish($id, __METHOD__ . ' expects $id to be an integer');
+        return $this->call('GET', "/shipping/services/{$id}.json");
     }
 
+    /**
+     * Get a list of Shipping Services associated with this App.
+     * @return array
+     */
+    public function getList()
+    {
+        return $this->call('GET', "/shipping/services.json");
+    }
 
+    /**
+     * Update a Shipping Service for the App.
+     * Note: an App may only update a Shipping Service that it owns.
+     * @param int $id The ID of the Shipping Service.
+     * @param array $properties The properties to update.
+     * @return array
+     */
     public function update($id, $properties)
     {
-
+        Assert::integerish($id, __METHOD__ . ' expects $id to be an integer');
+        Assert::isArray($properties, __METHOD__ . ' expects $properties to be supplied as an array');
+        if (!array_key_exists('shippingService', $properties)) {
+            throw new \InvalidArgumentException(__METHOD__ . ' expected a "shippingService" property');
+        }
+        return $this->call('PUT', "/shipping/services/{$id}.json", $properties);
     }
 
 }
