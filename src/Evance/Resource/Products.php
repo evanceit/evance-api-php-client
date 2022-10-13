23b3a25256a8765e8ae00fab3f6e4b2d51358fbb
kai <kaito.jacobson@azexis.com>
2022-10-13 10:17:19 +0100

    support products v2 api

--- src/Evance/Resource/Products.php
+++ src/Evance/Resource/Products.php
@@ -20,7 +20,7 @@ class Products extends AbstractResource
     public function get($id)
     {
         Assert::integerish($id, __METHOD__ . ' expects an $id as an integer');
-        return $this->call('GET', "/products/{$id}.json");
+        return $this->call('GET', "/{$this->version}/products/{$id}.json");
     }
 
     /**
@@ -30,7 +30,7 @@ class Products extends AbstractResource
     public function search(array $parameters = [])
     {
         Assert::isArray($parameters, __METHOD__ . ' expects $query to be supplied as an array of key value pairs');
-        return $this->call('GET', "/products.json", [], $parameters);
+        return $this->call('GET', "/{$this->version}/products.json", [], $parameters);
     }
 
     /**
@@ -42,7 +42,7 @@ class Products extends AbstractResource
         Assert::isArray($product, __METHOD__ . ' expects $product to be supplied as an array');
         Assert::keyExists($product, "product",  __METHOD__ . ' expects $product to contain key of "product"' .
             ' with value of object or array:');
-        return $this->call('POST', "/products.json", $product);
+        return $this->call('POST', "/{$this->version}/products.json", $product);
     }
 
     /**
@@ -56,7 +56,7 @@ class Products extends AbstractResource
         Assert::isArray($product, __METHOD__ . ' expects $product to be supplied as an array');
         Assert::keyExists($product, "product",  __METHOD__ . ' expects $product to contain key of "product"' .
             ' with value of json object');
-        return $this->call('PUT', "/products/{$id}.json", $product);
+        return $this->call('PUT', "/{$this->version}/products/{$id}.json", $product);
     }
 
     /**
@@ -66,6 +66,6 @@ class Products extends AbstractResource
     public function delete($id)
     {
         Assert::integerish($id, __METHOD__ . ' expects an $id as an integer');
-        return $this->call('DELETE', "/products/{$id}.json");
+        return $this->call('DELETE', "/{$this->version}/products/{$id}.json");
     }
 }
